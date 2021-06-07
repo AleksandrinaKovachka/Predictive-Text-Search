5ffdf66a27f4a87c4a095709c4c10de7f6e1a85d
Aleksandrina Kovachka <56196244+AleksandrinaKovachka@users.noreply.github.com>
2021-06-07 15:27:49 +0300

    Adds PredictiveText classes

---
+++ PredictiveTextSearch/PredictiveTextSearch.h
@@ -0,0 +1,18 @@
+//
+//  PredictiveTextSearch.h
+//  PredictiveTextSearch
+//
+//  Created by A-Team Intern on 7.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PredictiveTextSearch.
+FOUNDATION_EXPORT double PredictiveTextSearchVersionNumber;
+
+//! Project version string for PredictiveTextSearch.
+FOUNDATION_EXPORT const unsigned char PredictiveTextSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PredictiveTextSearch/PublicHeader.h>
+
+
